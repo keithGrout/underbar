c91a05ae709cb0d24023087dafb9547144afd1c3
keithGrout <keiwi.g@gmail.com>
2013-11-24 16:10:58 -0800

    _.map implemented

--- underbar.js
+++ underbar.js
@@ -143,6 +143,11 @@ _.uniq = function(array) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(array, function(value, index, collection){
+    	results.push(iterator(value));
+    })
+    return results;
   };
 
   /*
